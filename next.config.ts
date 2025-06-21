7408091148c2ac0b7aa1e48619bd3bc52ed38248
Noah Littlejohn <NoahJLittlejohn@gmail.com>
2025-06-21 14:49:45 +0000

    Moving project

--- next.config.ts
+++ next.config.ts
@@ -18,7 +18,8 @@ const nextConfig: NextConfig = {
   },
   allowedDevOrigins: [
     "https://6000-firebase-studio-1750101048344.cluster-lqnxvk7thvfw4wbonsercicksm.cloudworkstations.dev",
-    "https://9003-firebase-studio-1750101048344.cluster-lqnxvk7thvfw4wbonsercicksm.cloudworkstations.dev" // <-- ADD THIS LINE
+    "https://9003-firebase-studio-1750101048344.cluster-lqnxvk7thvfw4wbonsercicksm.cloudworkstations.dev", // <-- ADD THIS LINE
+    "https://9002-firebase-studio-1750101048344.cluster-lqnxvk7thvfw4wbonsercicksm.cloudworkstations.dev"
 ],  experimental: {
     // If there are other valid experimental options, they would go here.
     // For now, this might be empty if allowedDevOrigins was the only one.
